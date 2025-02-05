6042504f9d2f0fb65ce71f72740389ecafee156a
“Aiden <adnrdmnd@gmail.com>
2025-02-05 12:16:30 -0500

    init commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+    <div class="flex justify-between items-center mt-3 mx-6">
+        <RouterLink to="/" class="text-white font-martian-mono text-4xl">STRIDESPACE</RouterLink>
+        <div class="flex justify-between gap-4">
+            <RouterLink to="/about" class="font-albert-sans">about</RouterLink>
+            <RouterLink to="/" class="font-albert-sans">pricing</RouterLink>
+            <RouterLink to="/" class="font-albert-sans">login</RouterLink>
+        </div>
+    </div>
+    
+</template>
