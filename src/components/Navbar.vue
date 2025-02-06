da533e261f0b0dccfdc3ed960c726ba7827225f8
“Aiden <adnrdmnd@gmail.com>
2025-02-06 15:03:44 -0500

    temp homepage

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@ import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-    <div class="flex justify-between items-center mt-3 mx-3">
+    <div class="flex justify-between items-center mt-3">
         <RouterLink to="/" class="text-white font-martian-mono text-4xl">STRIDESPACE</RouterLink>
         <div class="flex justify-between gap-4">
             <RouterLink to="/about" class="font-albert-sans hover:text-purple transition-colors duration-250 ease-in-out">about</RouterLink>
@@ -11,5 +11,4 @@ import { RouterLink } from 'vue-router';
             <RouterLink to="/login" class="font-albert-sans hover:text-purple transition-colors duration-250 ease-in-out">login</RouterLink>
         </div>
     </div>
-    
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,46 @@
 </script>
 
 <template>
-    <main class="flex flex-col flex-1 justify-center items-center">
-        <h1 class="text-purple">home</h1>
+    <main class="flex flex-col flex-1">
+        <div class="flex mt-24">
+            <div class="flex flex-col">
+                <div class="flex items-start border-2 border-purple rounded-t-lg p-2">
+                    <h1 class="font-martian-mono text-8xl">STRIDESPACE</h1>
+                    <span class="text-purple text-8xl mt-[-10px]">*</span>
+                </div>
+                <h2 class="font-martian-mono text-4xl p-2 bg-purple text-black text-left rounded-b-lg">find new ways to see your runs</h2>
+                <p class="text-right opacity-25 font-martian-mono mt-1">*stride = running fast, space = where you find your people.</p>
+                
+                <!-- <div class="flex justify-around py-2 border-2 border-purple rounded-sm">
+                    <button class="rounded-sm font-martian-mono">signup</button>
+                    <button class="rounded-sm font-martian-mono">login</button>
+                </div> -->
+            </div>
+            <!-- <div class="p-2 mt-6">
+                    <h1 class="font-martian-mono text-2xl opacity-75">get started today?</h1>
+                </div>
+                <h2 class="font-martian-mono text-4xl p-2 bg-purple text-black text-left hover:bg-white hover:text-black transition-all duration-500 ease-in-out">signup</h2>
+                <h2 class="font-martian-mono text-4xl p-2 bg-purple text-black text-left hover:bg-white hover:text-black transition-all duration-500 ease-in-out">login</h2> -->
+        </div>
+
+        <div class="flex justify-around mt-20">
+            <div class="border-2 border-purple rounded-lg py-6 px-12">
+                <div class="font-martian-mono">visualize your running data</div>
+            </div>
+            <div class="border-2 border-purple rounded-lg py-6 px-12">
+                <div class="font-martian-mono">make custom art for each run</div>
+            </div>    
+            <div class="border-2 border-purple rounded-lg py-6 px-12">
+                <div class="font-martian-mono">find new communities</div>
+            </div>        
+        </div>
+
+        <div class="flex">
+            <div class="border-2 border-purple rounded-lg">
+                <div class="bg-white rounded-full p-8 opacity-50"></div>
+
+            </div>
+        </div>
+
     </main>
 </template>
